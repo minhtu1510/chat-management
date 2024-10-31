c578a158eaff0cae950ea589d14c73d9b5e51e66
minhtu1510 <thaithiminhtu.338@gmail.com>
2024-10-31 10:54:33 +0700

    Hiển thị data realtime

--- public/js/socket.js
+++ public/js/socket.js
@@ -19,6 +19,28 @@ if(formChat){
 
 // SERVER_RETURN_MESSAGE
 socket.on("SERVER_RETURN_MESSAGE",(data)=>{
-    console.log(data)
+    const myId = document.querySelector(".chat").getAttribute("my-id");
+    const body = document.querySelector(".chat .inner-body");
+    const div = document.createElement("div");
+    let htmlfullName = "";
+    if (myId == data.userId) {
+      div.classList.add("inner-outgoing");
+    } else {
+      div.classList.add("inner-incoming");
+      htmlfullName = `<div class="inner-name">${data.fullName}</div>`;
+    }
+
+    div.innerHTML = `
+    ${htmlfullName}
+    <div class="inner-content">${data.content}</div>
+    `;
+    body.appendChild(div);
 })
-// End SERVER_RETURN_MESSAGE
+// End SERVER_RETURN_MESSAGE\
+
+// Scroll Chat To Bottom
+const bodyChat = document.querySelector(".chat .inner-body");
+if(bodyChat) {
+  bodyChat.scrollTop = bodyChat.scrollHeight;
+}
+// End Scroll Chat To Bottom
